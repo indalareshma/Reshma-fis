e6f1a69dbf076ab0b3974d6b8b70fc9f9d452654
indalareshma <91862771+indalareshma@users.noreply.github.com>
2023-04-26 18:43:45 +0530

    Add files via upload

---
+++ gateway/src/main/java/com/example/microservices/gateway/GatewayConfigurations.java
@@ -0,0 +1,14 @@
+package com.example.microservices.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfigurations {
+	
+	
+	
+
+}
